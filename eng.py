d3df6a7212ebb17b842e559d40e9b27cfa1a6daf
theoldmoon0602 <theoldmoon0602@gmail.com>
2015-09-13 21:00:43 +0900

    obey flake8 modified

--- eng.py
+++ eng.py
@@ -1,4 +1,4 @@
-#coding: utf-8
+# coding: utf-8
 
 import random
 import re
@@ -6,17 +6,18 @@ import unittest
 import argparse
 import sys
 
+
 class Question:
     def __init__(self, english, japanese):
-        self.en = parse_english(english) # double dimention array
-        self.jp = japanese # str
+        self.en = parse_english(english)   # double dimention array
+        self.jp = japanese   # str
 
     def ask(self, level):
-        moth = {} # moth = ___
+        moth = {}   # moth = ___
         GREEN = "\033[92m"
         RED = "\033[91m"
         END = "\033[0m"
-        word_matcher = re.compile("\S+") # match to word
+        word_matcher = re.compile("\S+")  # match to word
 
         for v in random.sample(self.en, min(level, len(self.en))):
             # some are turned to moth
@@ -24,44 +25,41 @@ class Question:
                 moth[self.en.index(v)] = v
 
         # print problem
-        print(self.jp) # japanese
+        print(self.jp)  # japanese
         for i, w in enumerate(self.en):
             # english
             if i in moth:
                 print("_" * len(moth[i][0]), end=" ")
             else:
-                print(w[0], end = " ")
+                print(w[0], end=" ")
 
         print()
 
-        answers = word_matcher.findall(input()) # split to words
+        answers = word_matcher.findall(input())  # split to words
         corrects = 0
 
         for i, ans in enumerate(answers):
             if len(self.en) > i:
-                correct = ans in self.en[i] # are candidates include answer?
+                correct = ans in self.en[i]  # are candidates include answer?
                 if correct:
                     corrects += 1
                     print(GREEN + "O" + END, end="")
                 else:
-                    allcorrect = False
                     print(RED + "X" + END, end="")
                 print("{} {}".format(self.en[i], ans))
             else:
-                allcorrect = False
                 print(RED + "X" + END, end="")
                 print("[] {}".format(ans))
 
         if len(answers) < len(self.en):
             for v in self.en[len(answers):]:
-                print(RED + "X" + END,end="")
+                print(RED + "X" + END, end="")
                 print(v)
 
         if corrects == len(self.en):
             print(GREEN + "Correct!" + END)
         else:
             print(RED + "Wrong..." + END)
-
 
 
 def parse_paren(text):
@@ -77,12 +75,13 @@ def parse_paren(text):
         else:
             wbuf += c
 
+
 def parse_english(sentence):
     elements = []
     word = []
     wbuf = str()
     i = 0
-    sentence += " " # sentinel
+    sentence += " "  # sentinel
     while i < len(sentence):
         c = sentence[i]
         if c.isspace():
@@ -92,7 +91,7 @@ def parse_english(sentence):
             wbuf = str()
             word = []
         elif c == '(':
-            i += 1 # read '('
+            i += 1  # read '('
             index, choices = parse_paren(sentence[i:])
             i += index
             word = word + choices
@@ -100,7 +99,6 @@ def parse_english(sentence):
             wbuf += c
         i += 1
     return elements[:-1]
-
 
 
 def Questions(filename):
@@ -123,8 +121,12 @@ def Questions(filename):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-l", "--level", type=int, default=random.randint(0, sys.maxsize), help="The mothes increase with level. Default is random (almost cases level max")
-    parser.add_argument("-f", "--file", default="problems.txt", help="Set problem files path")
+    level_help = ('The mothes increase with level.'
+                  'Default is random (almost cases level max')
+    parser.add_argument("-l", "--level", type=int,
+                        default=sys.maxsize, help=level_help)
+    parser.add_argument("-f", "--file",
+                        default="problems.txt", help="Set problem files path")
     args = parser.parse_args()
 
     qs = Questions(args.file)
@@ -136,11 +138,18 @@ if __name__ == '__main__':
         if cont is not "":
             break
 
+
 class Test_parse_english(unittest.TestCase):
     def setUp(self):
-        self.sentences = ["What is your name?", "I (don't|do not) like it.","I think (that|) I am right."]
+        self.sentences = ["What is your name?",
+                          "I (don't|do not) like it.",
+                          "I think (that|) I am right."]
 
     def test_parse_english(self):
-        self.assertEqual(parse_english(self.sentences[0]), [["What"], ["is"], ["your"], ["name?"]])
-        self.assertEqual(parse_english(self.sentences[1]), [["I"], ["don't", "do not"], ["like"], ["it."]])
-        self.assertEqual(parse_english(self.sentences[2]), [["I"], ["think"], ["that", ""], ["I"], ["am"], ["right."]])
+        self.assertEqual(parse_english(self.sentences[0]),
+                         [["What"], ["is"], ["your"], ["name?"]])
+        self.assertEqual(parse_english(self.sentences[1]),
+                         [["I"], ["don't", "do not"], ["like"], ["it."]])
+        self.assertEqual(parse_english(self.sentences[2]),
+                         [["I"], ["think"], ["that", ""],
+                         ["I"], ["am"], ["right."]])
